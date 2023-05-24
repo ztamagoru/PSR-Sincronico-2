d34edd78de6dec51985c070214ccf9cdf9a2d648
tamagoru <marushannnn@outlook.com>
2023-05-24 18:20:18 -0300

    Add project files.

---
+++ Sincronico-2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Sincronico_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            tenorgif = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)tenorgif).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(102, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(68, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Maxwell";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(204, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(68, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // tenorgif
+            // 
+            tenorgif.Image = Properties.Resources.maxwell_the_cat_maxwell;
+            tenorgif.Location = new Point(57, 78);
+            tenorgif.Name = "tenorgif";
+            tenorgif.Size = new Size(261, 183);
+            tenorgif.SizeMode = PictureBoxSizeMode.Zoom;
+            tenorgif.TabIndex = 2;
+            tenorgif.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(375, 375);
+            Controls.Add(tenorgif);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Maxwell";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)tenorgif).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private PictureBox tenorgif;
+    }
+}
---
+++ Sincronico-2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Sincronico_1
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer player;
+
+        string imagepath = System.Reflection.Assembly.GetExecutingAssembly()
+               .Location + @"\..\..\..\..\Resources\maxwell-the-cat-maxwell.gif";
+
+        string soundpath = System.Reflection.Assembly.GetExecutingAssembly()
+                   .Location + @"\..\..\..\..\Resources\Maxwell the Cat Theme.wav";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            tenorgif.Visible = true;
+            PlaySound();
+
+            //await Task.Delay(43000);
+
+            await Wait();
+
+            MessageBox.Show("ｍａｘｗｅｌｌ");
+
+            tenorgif.Visible = false;
+            StopSound();
+        }
+
+        private async Task Wait()
+        {
+            await Task.Delay(43000);
+        }
+
+        private void PlaySound()
+        {
+            try
+            {
+                player = new SoundPlayer(soundpath);
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al reproducir el sonido." + ex.Message);
+            }
+        }
+
+        private void StopSound()
+        {
+            try
+            {
+                player.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al detener el sonido." + ex.Message);
+            }
+            finally
+            {
+                player.Dispose();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            tenorgif.Visible = false;
+            tenorgif.Image = Image.FromFile(imagepath);
+            RoundButtons(button1);
+            RoundButtons(button2);
+
+            player = new SoundPlayer(soundpath);
+        }
+
+        #region "meh, extras"
+        public Button RoundButtons(Button btn)
+        {
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderSize = 0;
+            btn.BackColor = Color.FromArgb(57, 62, 65);
+            btn.ForeColor = Color.White;
+            btn.Cursor = Cursors.Hand;
+
+            using (GraphicsPath Raduis = new GraphicsPath())
+            {
+                Raduis.StartFigure();
+
+                Raduis.AddArc(new Rectangle(0, 0, 20, 20), 180, 90);
+
+                Raduis.AddLine(10, 0, btn.Width - 20, 0);
+
+                Raduis.AddArc(new Rectangle(btn.Width - 20, 0, 20, 20), -90, 90);
+
+                Raduis.AddLine(btn.Width, 20, btn.Width, btn.Height - 10);
+
+                Raduis.AddArc(new Rectangle(btn.Width - 25, btn.Height - 25, 25, 25), 0, 90);
+
+                Raduis.AddLine(btn.Width - 10, btn.Width, 20, btn.Height);
+
+                Raduis.AddArc(new Rectangle(0, btn.Height - 20, 20, 20), 90, 90);
+
+                Raduis.CloseFigure();
+
+                btn.Region = new Region(Raduis);
+            }
+
+            return btn;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+    }
+}
